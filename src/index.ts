ff160396f96f22f00cb90ae7bfaca6294b072bfc
VisualSource <boomishere_network@outlook.com>
2021-12-21 12:47:41 -0500

    service worker work

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,17 @@ const game = new Game();
 let app = new App({
   target: document.body,
 });
+
+if(import.meta.env.PROD) {
+  if("serviceWorker" in navigator) {
+    if('Notification' in window) {
+      //https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications
+        Notification.requestPermission((status)=>{
+          console.log("Notification permission status", status);
+        });
+    }
+    window.addEventListener("load",()=>{
+      navigator.serviceWorker.register("/service-worker.js");
+    });
+  }
+}
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,7 +1,49 @@
 /// <reference lib="webworker" />
-import {clientsClaim} from 'workbox-core';
+import { clientsClaim, setCacheNameDetails } from "workbox-core";
+import { registerRoute } from 'workbox-routing';
+import { ExpirationPlugin } from "workbox-expiration";
+import {
+    NetworkFirst,
+    StaleWhileRevalidate,
+    CacheFirst,
+  } from 'workbox-strategies';
+
 
 declare const self: ServiceWorkerGlobalScope;
 
+
+self.addEventListener("message",(event)=>{
+    if(event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
 clientsClaim();
+
+setCacheNameDetails({
+    prefix: "polytopia",
+    suffix: "v1"
+});
+
+registerRoute( 
+    ({request})=> request.destination === "image", 
+    new CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+            })
+        ]
+    }) 
+);
+
+registerRoute(
+    ({request})=> request.destination === "script" || request.destination === "style",
+    new StaleWhileRevalidate({
+        cacheName: "static-resources"
+    })
+);
+
+
 
