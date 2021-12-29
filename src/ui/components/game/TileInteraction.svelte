db3126d4eda34e7dfafce9cf58ca52377418729a
VisualSource <boomishere_network@outlook.com>
2021-12-28 22:59:11 -0500

    ui work

--- src/ui/components/game/TileInteraction.svelte
+++ src/ui/components/game/TileInteraction.svelte
@@ -4,7 +4,7 @@
     import { Button, Icon } from 'sveltestrap';
     import { SystemEvents, ObjectEvents } from '../../../game/events/systemEvents';
 
-    let show = false;
+    let show = true;
     const events = new EventEmitter();
 
     const interaction = (event: any) => {
@@ -29,49 +29,80 @@
 </script>
 
 <style lang="sass">
-    .menu-interaction 
+    @use "../../_variables"
+    #tile-actions 
+        @include variables.flex-column()
         position: absolute
-        bottom: 5px 
-        left: 50%
-        background-color: var(--bs-dark)
+        bottom: 5px
+        right: 0
+        left: 0
+        margin: 0 auto
+        user-select: none
         color: var(--bs-light)
-        border-top-left-radius: 6px
+        width: 25%
+        height: 140px
+        border-top-left-radius: 6px 
         border-top-right-radius: 6px
-        height: 5rem
-        width: 25em
+        > main 
+            @include variables.flex-row-center()
+            height: 80px
+            gap: 20px
+            overflow-x: scroll
         > header 
-            display: flex
-            flex-direction: row
+            padding: 10px
+            display: flex 
             justify-content: space-between
-            align-content: center 
-            align-items: center
-            > div.desc-img
+            > div.preview 
                 display: flex
-                > div.title-desc
-                    display: flex
-                    flex-direction: column
-        
+                > div.preview-img 
+                    margin-right: 10px
+                > div.title-desc 
+                    @include variables.flex-column()
+                    > h6 
+                        margin-bottom: 1px
+                    > span 
+                        font-size: 0.875em
+                        word-wrap: anywhere
+                    
+    .tile-action 
+        @include variables.flex-column-center()
+        height: 50px
+        width: 50px
+        margin: 5px
+        user-select: none
+        &:active
+            transform: translate(1px,1px)
 
+    @media screen and (max-width: 765px) 
+        #tile-actions 
+            bottom: 0
+            width: 100%
 </style>
 
 {#if show} 
-    <div class="menu-interaction" in:fly="{{ y: 200, duration: 200 }}" out:fade>
+    <div id="tile-actions" class="bg-dark" in:fly="{{ y: 100, duration: 200 }}" out:fade>
         <header>
-            <div class="desc-img">
-                <div>
+            <div class="preview">
+                <div class="preview-img">
                     <img src="" alt="tile preview"/>
                 </div>
                 <div class="title-desc">
-                    <h6>TILE NAME</h6>
+                    <h6>
+                        <strong>TILE NAME</strong>
+                    </h6>
                     <span>TILE DESCRIPTION</span>
                 </div>
             </div>
             <div>
-                <Button on:click={onClose} class="rounded-circle">
-                    <Icon name="chevron-compact-down"/>
+                <Button on:click={onClose} class="rounded-circle bg-light">
+                    <Icon id="action" class="text-dark" name="chevron-compact-down"/>
                 </Button>
             </div>
         </header>
+        <main class="border-top border-light">
+            <div class="rounded-circle border border-3 bg-primary tile-action">A</div>
+            <div class="rounded-circle border border-3 bg-primary tile-action">B</div>
+        </main>
     </div>
 {/if}
 
